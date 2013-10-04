bf16c7792612d7eb59ccc15138c9233adfa241d9
黄勇 <huangyong.java@gmail.com>
2013-10-04 09:34:50 +0800

    【I】为模版方法增加 final 标识符

--- src/main/java/com/smart/sample/aspect/ProductActionAspect.java
+++ src/main/java/com/smart/sample/aspect/ProductActionAspect.java
@@ -15,34 +15,34 @@ public class ProductActionAspect extends BaseAspect {
     private long begin;
 
     @Override
-    protected void begin(Method method, Object[] args) {
+    public void begin(Method method, Object[] args) {
         if (logger.isInfoEnabled()) {
             logger.info("---------- Begin ----------");
         }
     }
 
     @Override
-    protected boolean filter(Method method, Object[] args) {
+    public boolean filter(Method method, Object[] args) {
         return method.getName().equals("getProducts");
     }
 
     @Override
-    protected void before(Method method, Object[] args) {
+    public void before(Method method, Object[] args) {
         begin = System.currentTimeMillis();
     }
 
     @Override
-    protected void after(Method method, Object[] args) {
+    public void after(Method method, Object[] args) {
         logger.info("Time: " + (System.currentTimeMillis() - begin) + "ms");
     }
 
     @Override
-    protected void error(Method method, Object[] args, Exception e) {
+    public void error(Method method, Object[] args, Exception e) {
         logger.info("Error: " + e.getMessage());
     }
 
     @Override
-    protected void end(Method method, Object[] args) {
+    public void end(Method method, Object[] args) {
         if (logger.isInfoEnabled()) {
             logger.info("----------- End -----------");
         }
