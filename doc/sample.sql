bda85f0f85c6e4e37c0467cbfa9e8c6d4066accd
黄勇 <huangyong.java@gmail.com>
2013-11-01 23:57:51 +0800

    【I】代码优化

--- doc/sample.sql
+++ doc/sample.sql
@@ -78,3 +78,20 @@ CREATE TABLE `user` (
 -- Records of user
 -- ----------------------------
 INSERT INTO `user` VALUES ('1', 'admin', 'admin');
+
+-- ----------------------------
+-- Table structure for customer
+-- ----------------------------
+DROP TABLE IF EXISTS `customer`;
+CREATE TABLE `customer` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `customer_name` varchar(50) DEFAULT NULL,
+  `description` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of customer
+-- ----------------------------
+INSERT INTO `customer` VALUES ('1', 'Customer 1', '客户 1');
+INSERT INTO `customer` VALUES ('2', 'Customer 2', '客户 2');
