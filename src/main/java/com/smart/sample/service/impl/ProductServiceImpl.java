c6b929ecf2db24a39df0b355ae635e359e7de07b
黄勇 <huangyong.java@gmail.com>
2013-12-23 18:15:37 +0800

    【I】无需将文件名放入 fieldMap 中

--- src/main/java/com/smart/sample/service/impl/ProductServiceImpl.java
+++ src/main/java/com/smart/sample/service/impl/ProductServiceImpl.java
@@ -21,6 +21,7 @@ public class ProductServiceImpl implements ProductService {
     @Override
     @Transaction
     public boolean createProduct(Map<String, Object> fieldMap, Multipart multipart) {
+        fieldMap.put("picture", multipart.getFileName());
         boolean result = DataSet.insert(Product.class, fieldMap);
         if (result) {
             UploadHelper.uploadFile(Tool.getBasePath(), multipart);
@@ -37,6 +38,7 @@ public class ProductServiceImpl implements ProductService {
     @Override
     @Transaction
     public boolean updateProduct(long id, Map<String, Object> fieldMap, Multipart multipart) {
+        fieldMap.put("picture", multipart.getFileName());
         boolean result = DataSet.update(Product.class, fieldMap, "id = ?", id);
         if (result) {
             UploadHelper.uploadFile(Tool.getBasePath(), multipart);
