4ea9cd5b541d439d5af386848082fa9521e4ed9c
黄勇 <huangyong.java@gmail.com>
2013-12-26 00:27:53 +0800

    【I】代码优化

--- src/test/java/com/smart/sample/test/UserServiceHessianTest.java
+++ src/test/java/com/smart/sample/test/UserServiceHessianTest.java
@@ -1,26 +1,22 @@
 package com.smart.sample.test;
 
-import com.caucho.hessian.client.HessianProxyFactory;
+import com.smart.plugin.hessian.HessianHelper;
 import com.smart.sample.entity.User;
 import com.smart.sample.service.UserService;
-import java.net.MalformedURLException;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class UserServiceHessianTest {
 
     @Test
-    public void loginTest() {
+    public void loginTest() throws Exception {
         String username = "admin";
         String password = "admin";
-        try {
-            String url = "http://localhost:8080/smart-sample/hessian/user_service";
-            HessianProxyFactory factory = new HessianProxyFactory();
-            UserService userService = (UserService) factory.create(UserService.class, url);
-            User user = userService.login(username, password);
-            Assert.assertNotNull(user);
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
+
+        String url = "http://localhost:8080/smart-sample/hessian/user_service";
+        UserService userService = HessianHelper.createClient(url, UserService.class);
+
+        User user = userService.login(username, password);
+        Assert.assertNotNull(user);
     }
 }
