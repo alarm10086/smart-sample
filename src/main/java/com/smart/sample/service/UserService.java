1e0db1aea2cf555a1563472a352375cf89eb3b20
黄勇 <huangyong.java@gmail.com>
2013-12-26 12:08:36 +0800

    【I】去掉 Hessian 相关代码

--- src/main/java/com/smart/sample/service/UserService.java
+++ src/main/java/com/smart/sample/service/UserService.java
@@ -1,9 +1,7 @@
 package com.smart.sample.service;
 
-import com.smart.plugin.hessian.Hessian;
 import com.smart.sample.entity.User;
 
-@Hessian("/user_service")
 public interface UserService {
 
     User login(String username, String password);
