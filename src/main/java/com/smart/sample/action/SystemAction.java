6d00566ade0f7323a7052dbbd32d6136920500b6
黄勇 <huangyong.java@gmail.com>
2013-12-30 16:26:18 +0800

    【F】提供一个简单的访问权限功能

--- src/main/java/com/smart/sample/action/SystemAction.java
+++ src/main/java/com/smart/sample/action/SystemAction.java
@@ -21,40 +21,33 @@ public class SystemAction {
 
     @Request("GET:/captcha")
     public void captcha() {
-        // 创建验证码
         String captcha = WebUtil.createCaptcha(DataContext.getResponse());
-        // 将验证码放入 Session 中
         DataContext.Session.put(Constant.CAPTCHA, captcha);
     }
 
     @Request("POST:/login")
     public Result login(Map<String, Object> fieldMap) {
-        // 获取请求参数
         String username = CastUtil.castString(fieldMap.get("username"));
         String password = CastUtil.castString(fieldMap.get("password"));
         String captcha = CastUtil.castString(fieldMap.get("captcha"));
-        // 判断验证码是否正确
+
         String sessionCaptcha = DataContext.Session.get(Constant.CAPTCHA);
         if (!sessionCaptcha.equals(captcha)) {
-            // 若验证码错误，则返回指定的失败标志
             return new Result(false).data("captcha_error");
         }
-        // 判断用户是否存在
+
         User user = userService.login(username, password);
         if (user == null) {
-            // 若用户不存在，则返回失败标志
             return new Result(false);
         }
-        // 将 用户ID 放入 Session 中
+
         DataContext.Session.put(Constant.USER_ID, user.getId());
-        // 从 Session 中获取 REDIRECT_URL，并判断 REDIRECT_URL 是否存在
         String redirectURL = DataContext.Session.get(Constant.REDIRECT_URL);
         if (StringUtil.isNotEmpty(redirectURL)) {
-            // 若 REDIRECT_URL 已存在，则首先从 Session 中移除 REDIRECT_URL，然后将 REDIRECT_URL 作为数据返回
             DataContext.Session.remove(Constant.REDIRECT_URL);
             return new Result(true).data(redirectURL);
         }
-        // 返回成功标志
+
         return new Result(true);
     }
 
--- src/main/java/com/smart/sample/aspect/AccessAspect.java
+++ src/main/java/com/smart/sample/aspect/AccessAspect.java
@@ -5,9 +5,9 @@ import com.smart.framework.annotation.Aspect;
 import com.smart.framework.annotation.Order;
 import com.smart.framework.exception.AccessException;
 import com.smart.framework.proxy.AspectProxy;
-import com.smart.framework.util.ArrayUtil;
 import com.smart.framework.util.WebUtil;
 import com.smart.sample.Constant;
+import com.smart.sample.action.SystemAction;
 import java.lang.reflect.Method;
 
 @Aspect(pkg = "com.smart.sample.action")
@@ -16,28 +16,18 @@ public class AccessAspect extends AspectProxy {
 
     @Override
     public boolean intercept(Class<?> cls, Method method, Object[] params) throws Exception {
-        // 获取类名与方法名
-        String className = cls.getSimpleName();
-        String methodName = method.getName();
-        // 排除特定类的特定方法（无需进行 AOP 拦截）
-        boolean result = true; // 默认为需要拦截
-        if (className.equals("SystemAction")) {
-            String[] methodNames = {"captcha", "login", "logout"};
-            if (ArrayUtil.contains(methodNames, methodName)) {
-                result = false; // 设置为无需拦截
-            }
+        boolean result = true;
+        if (cls == SystemAction.class) {
+            result = false;
         }
         return result;
     }
 
     @Override
     public void before(Class<?> cls, Method method, Object[] params) throws Exception {
-        // 从 Session 中获取 USER_ID，并判断 USER_ID 是否存在
         Long userId = DataContext.Session.get(Constant.USER_ID);
         if (userId == null) {
-            // 若 USER_ID 不存在（说明该用户未登录），则设置 REDIRECT_URL 到 Session 中
             WebUtil.setRedirectURL(DataContext.getRequest(), Constant.REDIRECT_URL);
-            // 抛出访问异常
             throw new AccessException();
         }
     }
