5f5eee6be69f7381dd452a91f922c4ef0ac7098f
黄勇 <huangyong.java@gmail.com>
2014-04-18 00:49:19 +0800

    【F】实现了一个简单的事务传播行为

--- src/main/java/com/smart/sample/service/impl/LogServiceImpl.java
+++ src/main/java/com/smart/sample/service/impl/LogServiceImpl.java
@@ -16,6 +16,10 @@ public class LogServiceImpl implements LogService {
         Log log = new Log();
         log.setDate(DateUtil.getCurrentDate());
         log.setTime(DateUtil.getCurrentTime());
+        log.setDescription(description);
         DataSet.insert(log);
+
+//        // 故意让事务回滚
+//        throw new RuntimeException();
     }
 }
