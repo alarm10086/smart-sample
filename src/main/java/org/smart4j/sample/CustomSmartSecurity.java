90fd69867ce8d615bd7d7c9721cd3131dbbb4575
黄勇 <huangyong.java@gmail.com>
2014-05-05 00:24:07 +0800

    【I】代码优化

--- src/main/java/org/smart4j/sample/CustomSmartSecurity.java
+++ src/main/java/org/smart4j/sample/CustomSmartSecurity.java
@@ -9,18 +9,18 @@ public class CustomSmartSecurity implements SmartSecurity {
     @Override
     public String getPassword(String username) {
         String sql = "select password from user where username = ?";
-        return DatabaseHelper.queryColumn(sql, username);
+        return DatabaseHelper.queryField(sql, username);
     }
 
     @Override
     public Set<String> getRoleNameSet(String username) {
         String sql = "select r.role_name from user u, user_role ur, role r where u.id = ur.user_id and r.id = ur.role_id and u.username = ?";
-        return DatabaseHelper.queryColumnSet(sql, username);
+        return DatabaseHelper.queryFieldSet(sql, username);
     }
 
     @Override
     public Set<String> getPermNameSet(String roleName) {
         String sql = "select p.permission_name from role r, role_permission rp, permission p where r.id = rp.role_id and p.id = rp.permission_id and r.role_name = ?";
-        return DatabaseHelper.queryColumnSet(sql, roleName);
+        return DatabaseHelper.queryFieldSet(sql, roleName);
     }
 }
