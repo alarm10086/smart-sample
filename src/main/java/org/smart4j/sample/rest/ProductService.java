2a5ed82f8e296aa8b9fef95728ee4c93721a01f5
黄勇 <huangyong.java@gmail.com>
2014-05-14 00:15:13 +0800

    【I】代码优化

--- src/main/java/org/smart4j/sample/rest/ProductService.java
+++ src/main/java/org/smart4j/sample/rest/ProductService.java
@@ -11,7 +11,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import org.smart4j.framework.dao.DataSet;
+import org.smart4j.framework.orm.DataSet;
 import org.smart4j.framework.tx.annotation.Service;
 import org.smart4j.framework.tx.annotation.Transaction;
 import org.smart4j.plugin.rest.Rest;
