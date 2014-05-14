2db03aea4b7cc6851d57392ce0be45288ff76ecd
黄勇 <huangyong.java@gmail.com>
2014-05-14 17:04:17 +0800

    【I】代码优化；提供 DataAccessor 接口及其默认实现

--- src/main/java/org/smart4j/sample/CustomSmartSecurity.java
+++ src/main/java/org/smart4j/sample/CustomSmartSecurity.java
@@ -1,5 +1,7 @@
 package org.smart4j.sample;
 
+import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Set;
 import org.smart4j.framework.dao.DatabaseHelper;
 import org.smart4j.security.SmartSecurity;
@@ -9,18 +11,20 @@ public class CustomSmartSecurity implements SmartSecurity {
     @Override
     public String getPassword(String username) {
         String sql = "select password from user where username = ?";
-        return DatabaseHelper.queryField(sql, username);
+        return DatabaseHelper.queryColumn(sql, username);
     }
 
     @Override
     public Set<String> getRoleNameSet(String username) {
-        String sql = "select r.role_name from user u, user_role ur, role r where u.id = ur.user_id and r.id = ur.role_id and u.username = ?";
-        return DatabaseHelper.queryFieldSet(sql, username);
+        String sql = "select distinct r.role_name from user u, user_role ur, role r where u.id = ur.user_id and r.id = ur.role_id and u.username = ?";
+        List<String> list = DatabaseHelper.queryColumnList(sql, username);
+        return new LinkedHashSet<String>(list);
     }
 
     @Override
     public Set<String> getPermNameSet(String roleName) {
-        String sql = "select p.permission_name from role r, role_permission rp, permission p where r.id = rp.role_id and p.id = rp.permission_id and r.role_name = ?";
-        return DatabaseHelper.queryFieldSet(sql, roleName);
+        String sql = "select distinct p.permission_name from role r, role_permission rp, permission p where r.id = rp.role_id and p.id = rp.permission_id and r.role_name = ?";
+        List<String> list = DatabaseHelper.queryColumnList(sql, roleName);
+        return new LinkedHashSet<String>(list);
     }
 }
